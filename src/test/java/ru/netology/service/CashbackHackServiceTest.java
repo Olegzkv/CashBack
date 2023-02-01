c46817ebf08864d597c23116daaf0887b72a2ec8
Oleg <zkv_testpost@mail.ru>
2023-02-01 18:17:37 +0300

    JUnitEdit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -10,32 +10,32 @@ public class CashbackHackServiceTest {
     @Test
     public void shouldCalculateIfAmountCorrect() {
         int amount = 5700;
-        int actual = service.remain(amount);
         int expected = 300;
+        int actual = service.remain(amount);
         assertEquals(actual,expected);
     }
 
     @Test
     public void shouldCalculateIfAmountInCorrect() {
         int amount = 5000;
-        int actual = service.remain(amount);
         int expected = 1000;
+        int actual = service.remain(amount);
         assertEquals(actual,expected);
     }
 
     @Test
     public void shouldCalculateIfAmountZero() {
         int amount = 0;
-        int actual = service.remain(amount);
         int expected = 1000;
+        int actual = service.remain(amount);
         assertEquals(actual,expected);
     }
 
     @Test
     public void shouldCalculateIfAmount1000() {
         int amount = 1000;
-        int actual = service.remain(amount);
         int expected = 0;
+        int actual = service.remain(amount);
         assertEquals(actual,expected);
     }
 }
